1dbaea5dbdb7c3d363111321afeb6d77bf8a8fbe
MinaKhalaf <52135282+MinaKhalaf@users.noreply.github.com>
2019-06-23 23:05:22 +0300

    Add files via upload

---
+++ ConstructoShopUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConstructoShopUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConstructoShopUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("161b6316-ec77-4f89-8415-d8b5df5d0928")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConstructoShopUnitTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConstructionEquipmentsSol;
+
+namespace ConstructoShopUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void CalculateHeavyRentalPrice()
+        {
+            var r = new Home();
+            double price =r.Fn_CalculateHeavyPrice(5);
+            Assert.AreEqual(400, price);
+        }
+        [TestMethod]
+        public void CalculateRegularRentalPrice()
+        {
+            var r = new Home();
+            double price = r.Fn_CalculateRegularPrice(5);
+            Assert.AreEqual(340, price);
+        }
+        [TestMethod]
+        public void CalculateSpecializedRentalPrice()
+        {
+            var r = new Home();
+            double price = r.Fn_CalculateSpecializedPrice(5);
+            Assert.AreEqual(260, price);
+        }
+        [TestMethod]
+        public void CalculateAddedPoints()
+        {
+            var r = new Home();
+            double points = r.fn_CalculatePoints(0,0,2,2,2);
+            Assert.AreEqual(3, points);
+        }
+    }
+}
