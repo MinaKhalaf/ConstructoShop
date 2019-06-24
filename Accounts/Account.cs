85f68029f4dbbbf70ae6e674a71d883935ce2802
MinaKhalaf <52135282+MinaKhalaf@users.noreply.github.com>
2019-06-24 16:09:06 +0300

    Add files via upload

---
+++ Accounts/Account.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    public class Account
+    {
+        private string[] Accounts = { "admin", "admin" };
+        static void Main(string[] args)
+        {
+            //bool w =Fn_ValidateUser("admin", "admin");
+        }
+        public bool Fn_ValidateUser(string User,string Pass)
+        {
+
+            if (Accounts[0] == User && Accounts[1] == Pass)
+            {
+                return true;
+            }
+            else return false;
+         
+        }
+    }
+}
